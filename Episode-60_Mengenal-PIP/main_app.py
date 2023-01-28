f9e706da98c745a0b60f1d9f9c114f59499cca7e
agungsetyonugrohoo <agungsn94@gmail.com>
2023-01-28 17:45:02 +0700

    mempelajari tentang PIP

---
+++ Episode-60_Mengenal-PIP/main_app.py
@@ -0,0 +1,12 @@
+"""
+PIP adalah suatu package resource library untuk menginstall package library. Perintah-perintah PIP diantaranya :
+- pip --version : untuk melihat version pip
+- pip list : untuk menunjukkan list package yang diinstall
+- python3 -m pip install --upgrade pip : untuk upgrade pip
+- pip install <nama_package> : untuk menginstall package tertentu
+- pip install <nama_package>==<no_version> : untuk menginstall package dengan versi tertentu
+- pip : untuk mengetahui perintah-perintah pada pip (seperti help)
+- pip uninstall <nama_package> : untuk menguninstall package tertentu
+
+PyPI merupakan kumpulan package library python yang dapat diinstal dan digunakan dalam project yang kita buat.
+"""
